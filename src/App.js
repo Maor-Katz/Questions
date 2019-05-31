58ad4e80bf8bfb3b7ee1ba84c98235e941efda14
maor-katz <maorkatz1990@gmail.com>
2019-05-31 12:26:46 +0300

    de

--- src/App.js
+++ src/App.js
@@ -17,8 +17,8 @@ class App extends React.Component {
             content: {
                 top: '50%',
                 left: '50%',
-                height: '30%',
-                width: '20%',
+                height: '13%',
+                width: '18%',
                 right: 'auto',
                 bottom: 'auto',
                 marginRight: '-50%',
